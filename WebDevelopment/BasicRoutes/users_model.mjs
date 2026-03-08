423d9f8d54335bce550e3da9ebffba25dd5ec3d9
shawn-singharaj <shawn.singharaj@gmail.com>
2026-03-08 16:41:20 -0700

    Basic route handling and MongoDB implementation

---
+++ WebDevelopment/BasicRoutes/users_model.mjs
@@ -0,0 +1,87 @@
+// Get the mongoose object
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+let connection = undefined;
+let Users = undefined
+
+/**
+ * This function connects to the MongoDB server.
+ */
+async function connect(){
+    try{
+        await mongoose.connect(process.env.MONGODB_CONNECT_STRING);
+        connection = mongoose.connection;
+        console.log("Successfully connected to MongoDB using Mongoose!");
+        Users = createModel();
+    } catch(err){
+        console.log(err);
+        throw Error(`Could not connect to MongoDB ${err.message}`)
+    }
+}
+
+function createModel(){
+    const usersSchema = mongoose.Schema({
+        name: {type: String, required: true},
+        age: {type: Number, required: true},
+        email: {type: String, required: true},
+        phoneNumber: {type: Number, required: false},
+    });
+    return mongoose.model('users', usersSchema);
+}
+
+async function createUser(name, age, email, phoneNumber){
+    const users = new Users({name: name, age: age, email: email, phoneNumber: phoneNumber});
+    return users.save();
+}
+
+const findUsers = async (filter) => {
+    const query = Users.find(filter);
+    return query.exec();
+};
+
+const findUsersById = async (_id) => {
+    try{
+        const user = await Users.findById(_id);
+        return user;
+    }catch(err){
+        throw err;
+    }
+};
+
+async function updateUser(_id, name, age, email, phoneNumber){
+    const result = await Users.findBy({_id: _id}, 
+        {name: name, age: age, email: email, phoneNumber: phoneNumber});
+    return result;
+}
+
+async function deleteUserById(_id) {
+    
+    try {
+        const result = await Users.deleteOne({ _id });
+
+        if (result.deletedCount === 1) {
+            return { message: "User deleted successfully" };
+        } else {
+            return { message: "User not found" };
+        }
+    } catch (err) {
+        throw err;
+    }    
+}
+
+async function deleteUser(filter) {
+    try {
+        const result = await Users.deleteOne(filter);
+
+        if (result.deletedCount === 1) {
+            return { message: "User deleted successfully" };
+        } else {
+            return { message: "User not found" };
+        }
+    } catch (err) {
+        throw err;
+    }
+}
+
+export { connect, createUser, findUsers, findUsersById, updateUser, deleteUserById, deleteUser };
