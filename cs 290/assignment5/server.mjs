6881fdf26e9d8cf99102b63f634350e07042364f
shawn-singharaj <140577720+shawn-singharaj@users.noreply.github.com>
2026-03-08 15:45:33 -0700

    Delete cs 290 directory

--- cs 290/assignment5/server.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const app = express();
-const PORT = 3000;
-
-app.use(express.static('public'));
-
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
-});
--- cs 290/assignment6/data/orders.mjs
+++
@@ -1,8 +0,0 @@
-import Order from '../order.mjs';
-
-const orders = [
-      new Order('ORCL', 87),
-      new Order('MSFT', 44)
-];
-
-export default orders;
